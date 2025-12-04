f1f314ae25215acd6aeb972f217c83c1cfc16778
unknown <mdtamimhossain1357@gmail.com>
2025-12-04 12:14:22 +0600

    issue solved for loading env

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,13 +1,22 @@
 import admin from "firebase-admin";
+import dotenv from "dotenv";
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      project_id: process.env.FIREBASE_PROJECT_ID,
-      client_email: process.env.FIREBASE_CLIENT_EMAIL,
-      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
-    })
-  });
+dotenv.config(); 
+
+const privateKey = process.env.FIREBASE_PRIVATE_KEY;
+
+console.log("privateKey", privateKey ? "loaded" : "undefined");
+
+if (!privateKey) {
+  throw new Error("FIREBASE_PRIVATE_KEY is missing in .env");
 }
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    private_key: privateKey.replace(/\\n/g, "\n"),
+  }),
+});
 
 export default admin;
